ff4891ae3b499cafa85395c4a01394010fee756b
Mikhail Zhalskiy <mikhail.zhalskiy@umbrellait.com>
2023-05-26 22:37:27 +0300

    added publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.0-beta03" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.21" apply false
+
+    `maven-publish`
 }
 
 //allprojects {
